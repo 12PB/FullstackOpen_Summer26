da44906a04dd5c9b466c288b18d4d7c37f24f375
Puneet <kvppss@gmail.com>
2026-05-06 16:59:29 +0800

    1.7 submission

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+const Header = ({title}) => {
+  return (
+    <div>
+    <h1>{title}</h1>
+    </div>
+  )
+}
+
+const Display = ({props}) => {
+  let good, neutral, bad = props
+  console.log({good})
+  return 
+}
+
+const Statistics = ({feedback}) => {
+  let [good, neutral, bad]= feedback
+  let count = good + neutral + bad
+  let avg = (good*1 + bad*-1) / count
+  let positive = good / count
+  return (
+    <div> 
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {count}</p>
+      <p>average {avg}</p>
+      <p>positive {positive}</p>
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const statistics = [good, neutral, bad]
+
+  const title = 'give feedback'
+  const subheader = 'statistics'
+
+  const goodIncrease = () => setGood(good + 1)
+  const neutralIncrease = () => setNeutral(neutral + 1)
+  const badIncrease = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <Header title={title}/>
+      <Button onClick={goodIncrease} text="good"/>
+      <Button onClick={neutralIncrease} text="neutral"/>
+      <Button onClick={badIncrease} text="bad"/>
+      <Header title={subheader}/>
+      <Statistics feedback={statistics}/>
+    </div>
+  )
+}
+
+export default App
