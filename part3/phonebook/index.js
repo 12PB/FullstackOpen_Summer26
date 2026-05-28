1dca1169319cb57ce798ff66163c43067f9ecaf8
Puneet <kvppss@gmail.com>
2026-05-28 10:58:28 +0800

    3.7 submission

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
@@ -81,7 +84,7 @@ app.post('/api/persons', (request, response) => {
     id: generateId(),
   }
 
-  const repeat = persons.find(person => person.name === body.name)
+  const repeat = persons.find(person => person.name.toLowerCase === body.name.toLowerCase)
 
   if (!repeat) {
     persons = persons.concat(person)
