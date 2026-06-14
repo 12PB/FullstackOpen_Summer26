251dc7ec507456bcb6ec1170094e961900092ded
Puneet <kvppss@gmail.com>
2026-06-14 21:33:54 +0800

    4.12 submission

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -14,61 +14,92 @@ beforeEach(async () => {
   await Blog.insertMany(testHelper.blogList)
 })
 
+describe('all blogs', () => {
+  test(' are returned', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, testHelper.blogList.length)
+  })
 
-test('blogposts are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+  test(' are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('have unique identifier field id', async () => {
+    const blogs = await api.get('/api/blogs')
+    assert('id' in blogs.body[0])
+  })
 })
 
-test('all blog are returned', async () => {
-  const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, testHelper.blogList.length)
-})
+describe('a new valid blog', () => {
+  test('can be added', async () => {
+    const newBlog = {
+      title: 'async/await simplifies making async calls',
+      author: 'Test Author',
+      url: 'testAddition',
+      likes: 5,
+    }
 
-test('unique identifier of blogposts is id', async () => {
-  const blogs = await api.get('/api/blogs')
-  assert('id' in blogs.body[0])
-})
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'async/await simplifies making async calls',
-    author: 'Test Author',
-    url: 'testAddition',
-    likes: 5,
-  }
+    const blogsAtEnd = await testHelper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, testHelper.blogList.length + 1)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const contents = blogsAtEnd.map(n => n.title)
+    assert(contents.includes('async/await simplifies making async calls'))
+  })
 
-  const blogsAtEnd = await testHelper.blogsInDb()
-  assert.strictEqual(blogsAtEnd.length, testHelper.blogList.length + 1)
+  test('autofills missing likes property', async () => {
+    const newBlog = {
+      title: 'async/await simplifies making async calls',
+      author: 'Test Author',
+      url: 'testAddition',
+    }
 
-  const contents = blogsAtEnd.map(n => n.title)
-  assert(contents.includes('async/await simplifies making async calls'))
-})
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-test('check if likes property exists', async () => {
-  const newBlog = {
-    title: 'async/await simplifies making async calls',
-    author: 'Test Author',
-    url: 'testAddition',
-  }
+    const blogs = await api.get('/api/blogs')
+    const createdBlog = blogs.body.find(blog => blog.title === 'async/await simplifies making async calls')
+    assert.equal(createdBlog.likes, 0)
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+  test('checks if title property exists', async () => {
+    const newBlog = {
+      author: 'Test Author',
+      url: 'testAddition',
+      likes: 5
+    }
 
-  const blogs = await api.get('/api/blogs')
-  const createdBlog = blogs.body.find(blog => blog.title === 'async/await simplifies making async calls')
-  assert.equal(createdBlog.likes, 0)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('checks if url property exists', async () => {
+    const newBlog = {
+      title: 'async/await simplifies making async calls',
+      author: 'Test Author',
+      likes: 5,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+  })
 })
 
 after(async () => {
