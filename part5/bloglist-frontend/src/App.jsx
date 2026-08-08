4e26e7ba2b141dc40ff03fd33058ced631fee7c5
Puneet <kvppss@gmail.com>
2026-08-08 11:43:00 +0800

    5.2 submission

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -17,10 +17,23 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async event => {
     event.preventDefault()
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -61,6 +74,11 @@ const App = () => {
     </div>
   )
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
   return (
     <div> 
       <Notification message={errorMessage} />
@@ -69,7 +87,10 @@ const App = () => {
       {user && 
         <div>
           <h2>blogs</h2>
-          {user.username} logged in
+          {user.username} logged in 
+          <button type="submit" onClick={() => handleLogout()}>
+            logout
+          </button>
           {blogs.map(blog => 
             <Blog key={blog.id} blog={blog} />
           )}
